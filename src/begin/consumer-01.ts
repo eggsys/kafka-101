e125889af3c2358e22bd585cdbe5891b31ce05ef
eggsys <egg.rkt2@gmail.com>
2022-04-24 01:31:29 +0700

    init

---
+++ src/begin/consumer-01.ts
@@ -0,0 +1,27 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+
+async function receive() {
+  const consumer = kafka.consumer({ groupId: "test-group" });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "topic-test", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log("Incoming =>", topic);
+      console.log({
+        value: message.value.toString(),
+      });
+    },
+  });
+}
+
+
+
+receive();
---
+++ src/begin/producer-01.ts
@@ -0,0 +1,30 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+async function send() {
+  const producer = kafka.producer();
+//   const admin = kafka.admin()                   //uncomment this if need to create Topic
+  await producer.connect();
+
+//   await admin.createTopics({
+//     // waitForLeaders: true,
+//     topics: [
+//       { topic: 'test-topic:1' },
+//     ],
+//   })
+
+  await producer.send({
+    topic: "topic-test",
+    messages: [{ value: "Hello KafkaJS user! V.2 5"}],
+  });
+
+  await producer.disconnect();
+}
+
+
+
+send();
